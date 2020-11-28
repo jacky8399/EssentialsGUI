dfc593fe79cafbc3cd93d1ba758f64c9e5b1056e
jacky8399 <jacky8399email@gmail.com>
2020-11-28 18:53:17 +0800

    Initial commit

---
+++ src/main/java/com/jacky8399/essentialsgui/EssentialsGUI.java
@@ -0,0 +1,17 @@
+package com.jacky8399.essentialsgui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EssentialsGUI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
